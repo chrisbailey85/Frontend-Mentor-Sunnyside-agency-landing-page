919f08f2625bc8bb5fc2aa45b835bf6f35d83ef6
Chris Bailey <chris85bailey@aol.co.uk>
2021-06-16 10:57:33 +0100

    mobile size done, click event add to arrow

--- dist/main.js
+++ dist/main.js
@@ -1,20 +1,32 @@
 const hamburgerBtn = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
-
+const arrow = document.querySelector('.arrow__img')
 // mobile nav 
+
+
 hamburgerBtn.addEventListener('click', () => {
     !hamburgerBtn.classList.contains('open') ? openNav() : closeNav();
 })
 
-
+arrow.addEventListener('click', arrrowScroll)
 function openNav() {
     hamburgerBtn.classList.toggle('open');
     hamburgerBtn.setAttribute('aria-expanded', 'true')
     nav.classList.toggle('open');
+    arrow.style.opacity = 0;
 }
 
 function closeNav() {
     hamburgerBtn.classList.toggle('open');
     hamburgerBtn.setAttribute('aria-expanded', 'false')
     nav.classList.toggle('open');
+    arrow.style.opacity = 1;
+}
+
+function arrrowScroll() {
+    let viewport = window.innerHeight;
+    window.scrollTo({
+        top: viewport,
+        behavior: 'smooth'
+    });
 }
